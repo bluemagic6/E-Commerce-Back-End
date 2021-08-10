f337fee38e8e46bbd6f586771762d052728c3995
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-08-10 16:15:57 -0500

    added category get one

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,35 +15,55 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne( {
+      where: {id: req.params.id},
+      // JOIN with locations, using the Trip through table
+      include: [Product]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+      const categoryData = await Category.create(req.body);
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-    // try {
-    //   const userData = await User.destroy({
-    //     where: {
-    //       id: req.params.id,
-    //     },
-    //   });
-    //   if (!userData) {
-    //     res.status(404).json({ message: 'No user with this id!' });
-    //     return;
-    //   }
-    //   res.status(200).json(userData);
-    // } catch (err) {
-    //   res.status(500).json(err);
-    // }
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No trip with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
