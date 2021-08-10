a206202c5d51bb9aea66fb560887c95195c1c666
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-08-09 21:58:11 -0500

    added category get route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,16 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -23,6 +30,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+    // try {
+    //   const userData = await User.destroy({
+    //     where: {
+    //       id: req.params.id,
+    //     },
+    //   });
+    //   if (!userData) {
+    //     res.status(404).json({ message: 'No user with this id!' });
+    //     return;
+    //   }
+    //   res.status(200).json(userData);
+    // } catch (err) {
+    //   res.status(500).json(err);
+    // }
 });
 
 module.exports = router;
